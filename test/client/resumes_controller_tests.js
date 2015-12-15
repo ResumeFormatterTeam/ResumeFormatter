94cb9e612709f8c0c5434efd7194aa426b69cc2a
Michael Tse <michael.tse3@gmail.com>
2015-12-15 11:38:49 -0800

    designed resume controller tests tests for angular crud using karma

--- test/client/resumes_controller_tests.js
+++ test/client/resumes_controller_tests.js
@@ -1,2 +1,74 @@
 require(__dirname + '/../../app/js/entry');
 require('angular-mocks');
+
+describe('Resumes Controller', function() {
+  var $httpBackend;
+  var $ControllerConstructor;
+  var $scope;
+
+  beforeEach(angular.mock.module('resumeApp'));
+
+  beforeEach(angular.mock.inject(function($rootScope, $controller) {
+    $scope = $rootScope.$new();
+    $ControllerConstructor = $controller;
+  }));
+
+  it('should be able to create a controller', function() {
+    var controller = $ControllerConstructor('ResumesController', {$scope:$scope});
+    expect(typeof $scope).toBe('object');
+    expect(typeof controller).toBe ('object');
+    expect(Array.isArray($scope.resume)).toBe(true);
+  });
+
+  describe('REST request functions', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $ControllerConstructor('ResumesController', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should make a get request when getAll() is called', function() {
+      $httpBackend.expectGET('/api/resume').respond(200, [{_id: 1, firstName: 'Goldilocks', lastName: '3Bears'}]);
+      $scope.getAll();
+      $httpBackend.flush();
+      expect($scope.resume[0].firstName).toBe('Goldilocks');
+      expect($scope.resume[0].lastName).toBe('3Bears');
+    });
+
+    it('should be able to create a new resume', function() {
+      //might need full schema when posting
+      $httpBackend.expectPOST('/api/resume', {firstName: 'Teddy', lastName: 'Bear'}).respond(200, {firstName: 'Theodore', lastName: 'Roosevelt'});
+      expect($scope.resume.length).toBe(0);
+      expect($scope.newResume).toEqual($scope.defaults);
+      $scope.newResume.firstName = 'Panda';
+      $scope.create($scope.newResume);
+      $httpBackend.flush();
+      expect($scope.resume[0].firstName).toBe('Theodore');
+      expect($scope.newResume).toEqual($scope.defaults);
+    });
+
+    it('should be able to update a resume', function() {
+      var resume = { firstName: 'Grizz', lastName: 'Bear',_id: 1, editing: true};
+      $httpBackend.expectPUT('/api/resume/1', resume).respond(200);
+      $scope.updateResume(resume);
+      $httpBackend.flush();
+      expect(resume.editing).toBe(false);
+    });
+
+    it('should be able to delete a resume', function() {
+      var resume = { firstName: 'Polar', lastName: 'Bear',_id: 1, editing: true};
+      $scope.resume = [resume];
+      $httpBackend.expectDELETE('/api/resume/1', resume).respond(200);
+      expect($scope.resume.length).toBe(0);
+      expect($scope.resume.indexOf(resume)).toBe(-1);
+    });
+
+  });
+})
+
+
