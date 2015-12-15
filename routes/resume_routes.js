de011a43fdb50ca22f6e4eeb58d484efe2c479e6
Emily Landi <emily.landi@gmail.com>
2015-12-15 11:32:29 -0800

    added resume schema and routes, minor edits to index.html ids and added routes in server.js

---
+++ routes/resume_routes.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Resume = require(__dirname + '/../models/resume');
+var handleError = require(__dirname + '/../lib/handleError');
+
+var resumesRouter = module.exports = exports = express.Router();
+
+//get
+resumesRouter.get('/resumes', function(req, res) {
+  Resume.find({}, function(err, data) {
+    if (err) return handleError(err, res);
+    res.json(data);
+  });
+});
+
+//create
+resumesRouter.post('/resumes', bodyParser.json(), function(req, res) {
+  var newResume = new Resume(req.body);
+  newResume.save(function(err, data) {
+    if (err) return handleError(err, res);
+    res.json(data);
+  });
+});
+
+//update
+resumesRouter.put('/resumes/:id', bodyParser.json(), function(req, res) {
+  var resumeData = req.body;
+  delete resumeData._id;
+  Resume.update({_id: req.params.id}, resumeData, function(err) {
+    if (err) return handleError(err, res);
+    res.json({msg:'Update successful!'});
+  });
+});
+
+//delete
+resumesRouter.delete('/resumes/:id', function(req, res) {
+  Resume.remove({_id: req.params.id}, function(err) {
+    if (err) return handleError(err, res);
+    res.json({msg:'Delete successful!'});
+  });
+});
